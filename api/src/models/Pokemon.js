0f6a2686f82fe3e4846613c16a044d3dd5267687
Diaz027 <dami27cito@gmail.com>
2023-10-20 23:27:06 -0300

    modulos

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,37 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    image: {
+      type: DataTypes.STRING,
+      defaultValue: ''
+    },
+    life_span: {
+      type: DataTypes.NUMBER,
+      allowNull: false,
+    },
+    height: {
+      type: DataTypes.NUMBER,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.NUMBER,
+      allowNull: false,
+    },
+
+    
+// Ataque. *
+// Defensa. *
+// Velocidad.
+
   });
 };
