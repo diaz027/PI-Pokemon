a4f55832a0323efd451339fed6399e690f51d8b5
Diaz027 <dami27cito@gmail.com>
2023-11-03 14:37:57 -0300

    w

--- api/src/controllers/getType.js
+++ api/src/controllers/getType.js
@@ -6,8 +6,7 @@ const getType = async () => {
     const result = typeApi.data;
 
     for (const typeData of result.results) {//itera sobre cada elemento en el arreglo result.results
-        //Para cada iteración del bucle, typeData tomará el valor del elemento actual del arreglo
-        // y puedes acceder a sus propiedades.
+        
         const typeName = typeData.name;//se utiliza para acceder al nombre de cada tipo dentro del bucle.
 
         const existingType = await Type.findAll({ where: { name: typeName } });
