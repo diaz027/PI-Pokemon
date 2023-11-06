ca905c498780efbe879a22a0b2dac671fdc602e2
Diaz027 <dami27cito@gmail.com>
2023-11-06 20:52:18 -0300

    t

--- api/src/controllers/getPokemonName.js
+++ api/src/controllers/getPokemonName.js
@@ -27,7 +27,7 @@ const getName = async (name) => {
     const pokemonFund = {
         id: pokemon.id,
         name: pokemon.name,
-        image: pokemon.sprites.front_default,
+        image: pokemon.sprites.other.dream_world.front_default,
         hp: pokemon.stats[0]["base_stat"],
         attack: pokemon.stats[1]["base_stat"],
         defense: pokemon.stats[2]["base_stat"],
